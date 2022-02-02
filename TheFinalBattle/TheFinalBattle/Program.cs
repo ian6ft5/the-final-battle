ad4a4960295943d7085135611588b5c1733dacd0
ian6ft5 <27964075+ian6ft5@users.noreply.github.com>
2022-02-02 13:09:19 -0700

    first commit

---
+++ TheFinalBattle/TheFinalBattle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Character
+{
+    public string Name { get; set; }
+}
+
+public class Skeleton : Character
+{
+    public Skeleton()
+    {
+        Name = "Skeleton";
+    }
+    public void DoNothing()
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine("");
+    }
+}
+
+public class Party
+{
+    List<Character> Characters = new List<Character>();
+    public bool IsPlayer { get; set; }
+
+    public Party(bool x)
+    {
+        IsPlayer = x;
+    }
+
+    public void AddToParty(Character newMember)
+    {
+        Characters.Add(newMember);
+    }
+}
